b7655cbe8938bbaebcab462690b4b3f437692cda
Julian Nicholls <juliannicholls29@gmail.com>
2016-01-28 13:52:36 +0000

    Add update of driver location for rider.

--- ParseStarterProject/RiderViewController.swift
+++ ParseStarterProject/RiderViewController.swift
@@ -20,6 +20,9 @@ class RiderViewController: UIViewController, MKMapViewDelegate, CLLocationManage
 
     var requestsPresent = 0
 
+    var latDelta  = 0.003
+    var longDelta = 0.003
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -33,20 +36,78 @@ class RiderViewController: UIViewController, MKMapViewDelegate, CLLocationManage
 
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let user : CLLocation = locations[0]
+        let driverPin = MKPointAnnotation()
 
         location = CLLocationCoordinate2DMake(user.coordinate.latitude, user.coordinate.longitude)
-        let span = MKCoordinateSpanMake(0.003, 0.003)
-        let region = MKCoordinateRegionMake(location, span)
+
+        let reqQuery = PFQuery(className: "RideRequest")
+
+        reqQuery.whereKey("username", equalTo: PFUser.currentUser()!.username!)
+
+        reqQuery.findObjectsInBackgroundWithBlock {
+            (objects, error) -> Void in
+
+            if error ==  nil {
+                let objects = objects!
+
+                for request in objects {
+                    if let driver = request["driverResponded"] {
+                        let locQuery = PFQuery(className: "DriverLocation")
+
+                        locQuery.whereKey("username", equalTo: driver)
+
+                        locQuery.findObjectsInBackgroundWithBlock({
+                            (objects, error) -> Void in
+
+                            if error == nil {
+                                let objects = objects!
+
+                                let driverLoc = objects[0]["location"]
+                                let driverCLL = CLLocation(latitude: driverLoc.latitude, longitude: driverLoc.longitude)
+                                let distance  = user.distanceFromLocation(driverCLL)
+                                let distkm = round(Double(distance / 100.0)) / 10.0
+
+                                self.callButton.setTitle("\(driver) is \(distkm)km away", forState: .Normal)
+
+                                driverPin.coordinate = CLLocationCoordinate2DMake(driverLoc.latitude, driverLoc.longitude)
+                                driverPin.title = "\(driver) Location"
+
+                                let distLat  = abs(driverLoc.latitude - self.location.latitude)
+                                let distLong = abs(driverLoc.longitude - self.location.longitude)
+
+                                self.latDelta  = distLat * 2 + 0.01
+                                self.longDelta = distLong * 2 + 0.01
+                            }
+                            else {
+                                print(error?.localizedDescription)
+                            }
+                        })
+                    }
+                    else {
+                        print("request present, no driver")
+                    }
+                }
+            }
+            else {
+                print(error?.localizedDescription)
+            }
+        }
+
+        let span    = MKCoordinateSpanMake(latDelta, longDelta)
+        let region  = MKCoordinateRegionMake(location, span)
 
         self.map.setRegion(region, animated: true)
 
-        let ann = MKPointAnnotation()
+        let userPin = MKPointAnnotation()
 
-        ann.coordinate = location
-        ann.title = "You are here!"
+        userPin.coordinate = location
+        userPin.title = "You are here!"
 
         map.removeAnnotations(map.annotations)
-         map.addAnnotation(ann)
+        map.addAnnotation(userPin)
+        if driverPin.title != "" {
+            map.addAnnotation(driverPin)
+        }
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
